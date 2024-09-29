f785e0dca447bb5bc9c0e208b33e968e00411964
Saksh02 <ss.sakshamsrivastava02@gmail.com>
2024-09-29 11:50:07 +0530

    text-based adventure game added

---
+++ JS/hello.js
@@ -0,0 +1,25 @@
+/*
+    let askName = require("prompt-sync")()
+    const name = askName("Move left or right? ")
+    console.log(name);
+
+    let doYouLikeThisCity = require("prompt-sync")()
+    let cityExperience = doYouLikeThisCity("Is this city really worth exploring: ")
+    console.log(cityExperience);
+*/
+
+const sayYourName = require("prompt-sync")()
+const yourName = sayYourName("What is your name? ")
+console.log("Hello", yourName, "how are you?");
+
+/* condition statements 
+==
+===
+!=
+!==
+<=
+>=
+<
+>
+*/
+
---
+++ JS/text-based_game.js
@@ -0,0 +1,38 @@
+let prompt = require("prompt-sync")()
+const nameOfTheUser = prompt("What is your name? ")
+console.log("Hello", nameOfTheUser.toUpperCase(),"welcome to the Game!");
+
+const wannaPlay = prompt("Do you want to play? ")
+
+if (wannaPlay.toLowerCase() === "yes") {
+//    Game logic begins here
+
+    const directionToGo = prompt("Hey, you entered in a building, you have two choices.Either Go left or right. Where will you go? ");
+    if (directionToGo.toLowerCase() === "left"){
+        console.log("You went left")
+        const againDirections = prompt("Keep going, now there is a pole, do you want to climb it? ")
+        if (againDirections === "no") {
+            console.log("You chose, not to climb")
+            const directions = prompt("Now, either go straight or stay here? ")
+            if (directions.toLowerCase() === "straight"){
+                console.log("Congratulations... you win!");
+            } else {
+                console.log("Sorry to hear that, but you lost...")
+            }
+        } else{
+            console.log("Sadly, you lost, the pole was weak and it felt\nAs soon as you tried to climb");
+            
+        }
+    } else if (directionToGo.toLowerCase() === "right") {
+        console.log("You went right and there was a big tubewell...")
+    }
+    else {
+        console.log("Provide correct input...")
+    }
+    
+} else if (wannaPlay.toLowerCase() === "no") {
+    console.log("Sorry to hear that :(");
+} else {
+    console.log("Invalid input given ...");  
+}
+
